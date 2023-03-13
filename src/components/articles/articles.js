4d8307120b4f4286dbb2cdd8fbb70a2daac792ce
gojimork <crusejohen@gmail.com>
2023-03-13 21:13:18 +0300

    refactoring article-list

--- src/components/articles/articles.js
+++ src/components/articles/articles.js
@@ -1,16 +1,15 @@
 import classes from "./articles.module.scss";
-import { v4 as uuidv4 } from "uuid";
-import { format } from "date-fns";
+
 import { useEffect, useState, useCallback } from "react";
 import { useHistory } from "react-router-dom";
 
-import { Avatar, Pagination } from "antd";
-import Like from "../like";
-import Tags from "../tags";
+import ArticleList from "../article-list/";
+import { Pagination } from "antd";
 
 const Articles = ({ page }) => {
   const history = useHistory();
   const [data, setData] = useState({ articles: [] });
+  // const [loading, setLoading] = useState(true);
 
   const getArticles = useCallback(async () => {
     const offset = (page - 1) * 5;
@@ -24,39 +23,9 @@ const Articles = ({ page }) => {
     getArticles();
   }, [getArticles]);
 
-  const articlesList = data.articles.map(
-    ({ title, description, tagList, favoritesCount, updatedAt, author }) => {
-      const { username, image } = author;
-      const id = uuidv4();
-      return (
-        <li key={id} className={classes["article-item"]}>
-          <div className={classes["article-item__content"]}>
-            <div className={classes["article-item__header"]}>
-              <h5 className={classes["article-item__title"]}>{title}</h5>
-              <Like favoritesCount={favoritesCount} />
-            </div>
-            <Tags tagList={tagList} />
-            <article className={classes["article-item__text"]}>
-              {description}
-            </article>
-          </div>
-          <div className={classes["article-item__signature"]}>
-            <div className={classes["signature"]}>
-              <span className={classes["signature__name"]}>{username}</span>
-              <span className={classes["signature__date"]}>
-                {format(new Date(updatedAt), "LLLL d, y")}
-              </span>
-            </div>
-            <Avatar size={46} src={image} />
-          </div>
-        </li>
-      );
-    }
-  );
-
   return (
     <div className={classes["articles-wrap"]}>
-      <ul className={classes["article-list"]}>{articlesList}</ul>
+      <ArticleList data={data.articles} />
       <Pagination
         current={page}
         total={data.articlesCount}
