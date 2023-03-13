32c2db99b045ad4179f05ddba61d352d39f9f674
gojimork <crusejohen@gmail.com>
2023-03-13 21:34:27 +0300

    add error handling

--- src/components/articles/articles.js
+++ src/components/articles/articles.js
@@ -1,31 +1,46 @@
 import classes from "./articles.module.scss";
-
 import { useEffect, useState, useCallback } from "react";
 import { useHistory } from "react-router-dom";
 
 import ArticleList from "../article-list/";
-import { Pagination } from "antd";
+import { Pagination, Spin, Alert } from "antd";
 
 const Articles = ({ page }) => {
-  const history = useHistory();
   const [data, setData] = useState({ articles: [] });
-  // const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   const getArticles = useCallback(async () => {
-    const offset = (page - 1) * 5;
-    const url = `https://api.realworld.io/api/articles?limit=5&offset=${offset}`;
-    const response = await fetch(url);
-    const body = await response.json();
-    setData(body);
+    setLoading(true);
+    try {
+      const offset = (page - 1) * 5;
+      const url = `https://api.realworld.io/api/articles?limit=5&offset=${offset}`;
+      const response = await fetch(url);
+      const body = await response.json();
+      setData(body);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
   }, [page]);
 
   useEffect(() => {
     getArticles();
   }, [getArticles]);
 
+  const history = useHistory();
   return (
     <div className={classes["articles-wrap"]}>
-      <ArticleList data={data.articles} />
+      {loading ? <Spin /> : <ArticleList data={data.articles} />}
+      {error ? (
+        <Alert
+          message={error.name}
+          description={error.message}
+          type="error"
+          showIcon
+        />
+      ) : null}
       <Pagination
         current={page}
         total={data.articlesCount}
