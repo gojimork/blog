3bcca048d26e6934769ef7bf642a0395c28467da
gojimork <crusejohen@gmail.com>
2023-03-17 10:45:43 +0300

    render full details from article

--- src/components/article-details/article-details.js
+++ src/components/article-details/article-details.js
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
-import markdownFile from "./body.md";
 import classes from "./article-details.module.scss";
 import ArticleHeader from "../article-header";
 
 const ArticleDetails = ({ slug }) => {
-  const [markdown, setMarkdown] = useState("");
   const [details, setDetails] = useState(null);
 
   useEffect(() => {
@@ -16,19 +14,11 @@ const ArticleDetails = ({ slug }) => {
     );
   }, [slug]);
 
-  useEffect(() => {
-    fetch(markdownFile)
-      .then((response) => response.text())
-      .then((text) => {
-        setMarkdown(text);
-      });
-  }, []);
-
   return (
     <div>
       <ArticleHeader details={details} />
       <ReactMarkdown className={classes["article-ditails"]}>
-        {markdown}
+        {details ? details.body : null}
       </ReactMarkdown>
     </div>
   );
